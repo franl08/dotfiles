c2732bd26aa28c1deb8a5995ad826024fb5adbaa
franl08 <franeves08@gmail.com>
2023-07-29 13:43:14 +0100

    Updated lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,8 +5,8 @@ return {
         options = {
             icons_enabled = true,
             theme = "auto",
-            component_separators = "|",
-            section_separators = ""
+            component_separators = "",
+            section_separators = { left = '', right = '' },
         },
     },
 }
